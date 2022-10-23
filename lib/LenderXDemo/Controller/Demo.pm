f214051fe913646a60a461b63b596a4f12213b73
Brad Barden <b@os13.org>
2022-10-23 19:19:13 +0000

    Controller::Demo: add postback and manage_events
    add an endpoint for event subscription postback and a consolidated endpoint for
    event subscription management
    
    remove debug code from list_events, and use openapi renderer rather than json

--- lib/LenderXDemo/Controller/Demo.pm
+++ lib/LenderXDemo/Controller/Demo.pm
@@ -1,20 +1,46 @@
 package LenderXDemo::Controller::Demo;
 use Mojo::Base 'Mojolicious::Controller';
 
-sub add_event {
+sub postback {
+  my $c = shift;
+  $c->app->log->debug('subscription event: '.$c->tx->req->body);
+  $c->render(text => 'OK');
 }
 
 sub list_events {
   my $c = shift;
-  die 'ok';
-  my $tx = $c->client->list_events;
-  $c->render(json => $tx->result->json);
+  my $tx = $c->app->client->list_events;
+  $c->render(openapi => $tx->result->json);
 }
 
-sub update_event {
+sub manage_events {
+  my $c = shift;
+  return unless $c->openapi->valid_input;
+  my $j = $c->req->json;
+  my $client = $c->app->client;
+  my (@added, @removed);
+  for my $add (@{$j->{add} || []}) {
+    my $tx = $client->add_async_event({}, json => {events => $add->{events}, url => $add->{url}});
+    return $c->render(openapi => $tx->res->json, status => $tx->res->code) if $tx->result->is_error;
+    push(@added, $tx->res->json);
+  }
+  for my $remove (@{$j->{remove} || []}) {
+    # no operationId provided for removing subscriptions.
+    my $url = $client->base_url->clone;
+    $url->path->trailing_slash(1);
+    $url->path("event/subscription/$remove");
+    my $token = $client->token;
+    my $tx = $client->ua->delete($url, {Authorization => "$token->{token_type} $token->{access_token}"});
+    return $c->render(openapi => $tx->res->json, status => $tx->res->code) if $tx->result->is_error;
+    push(@removed, $tx->res->json);
+  }
+  $c->render(openapi => {added => \@added, removed => \@removed});
 }
 
-sub remove_event {
+sub test_order {
+  my $c = shift;
+  my $tx = $c->app->client->test_order;
+  return $c->render(openapi => $tx->res->json, status => $tx->res->code);
 }
 
 1;
